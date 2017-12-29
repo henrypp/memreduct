49533e1d6841ecd6dc07c958f7debd0a44bdd641
henrypp <support@henrypp.org>
2017-12-29 20:22:56 +0600

    fixes

--- build_resources.bat
+++ build_resources.bat
@@ -2,5 +2,7 @@
 
 php "..\builder\make_resource.php" ".\src\resource.hpp"
 php "..\builder\make_locale.php" "Mem Reduct" "memreduct" ".\bin\i18n" ".\src\resource.hpp" ".\bin\memreduct.lng"
+copy /y ".\bin\memreduct.lng" ".\bin\32\memreduct.lng"
+copy /y ".\bin\memreduct.lng" ".\bin\64\memreduct.lng"
 
 pause
