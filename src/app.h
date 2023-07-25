169775585d8bef02402fe050ff25f27a8215eacc
henrypp <support@henrypp.org>
2023-07-25 14:25:41 +0600

    ++version

--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"Mem Reduct"
 #define APP_NAME_SHORT L"memreduct"
-#define APP_VERSION L"3.4"
-#define APP_VERSION_RES 3,4,0,0
+#define APP_VERSION L"3.4.1"
+#define APP_VERSION_RES 3,4,1,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2011-2023 " APP_AUTHOR L". All Rights Reversed."
